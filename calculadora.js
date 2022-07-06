1479ffcc4a0f09c195909a6930c9085752cc7f3e
Exe <exeeroot@gmail.com>
2022-07-06 08:59:26 -0400

    Inicio de repositorio

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+export function suma(a,b){
+    return a + b
+}
+
+export function resta(a,b){
+    return a - b
+}
+
+export default function owo(){
+    return 'owo'
+}
+
+export const a = 1
---
+++ index.js
@@ -0,0 +1,14 @@
+function sinPromesa(){
+    return Promise.resolve(1)
+    .then(x => x+1)
+    .then(x => x+1)
+    .then(console.log)
+}
+
+async function conPromesa(){
+
+    const resultado = await Promise.resolve(1)
+    console.log(resultado + 1 + 1)
+}
+
+conPromesa()
