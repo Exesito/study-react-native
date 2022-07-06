d5049b2edc7501b09804cbcc238744efd2bd7205
Exe <exeeroot@gmail.com>
2022-07-06 18:39:40 -0400

    Studied the section 5 of the course, actually step on 6.1

--- 4 Comenzamos/HolaMundo/App.js
+++ 4 Comenzamos/HolaMundo/App.js
@@ -1,52 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 
-const Texto = ({style}) => {
-  const [texto, setTexto] = useState("Hola Mundo!");
-  const actualizaTexto = () => {
-    setTexto('Chao Mundo')
+
+export default function App() {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+      setUsers(data)
+      setLoading(false)
+    })
+  }, [])
+
+  if(loading){
+    return <View style={styles.center}><Text>Cargando...</Text></View>
   }
 
   return (
-    <Text style={ [styles.text, style] } onPress={actualizaTexto}>{ texto }</Text>
-  )
-}
-
-export default function App() {
-  return (
     <View style={styles.container}>
-
-      <Texto style={styles.red}/>
-      <Texto style={styles.blue}/>
-      <Texto style={styles.green}/>
-
-      <StatusBar style="auto" />
+      <ActivityIndicator size="large" color='#0000ff'></ActivityIndicator>
+      <FlatList data = {users}
+      renderItem = {({item}) => <Text style={styles.item}>{item.name}</Text>}
+      keyExtractor={item => String(item.id)}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 24,
-    color: 'white',
-    height: 100,
-    width: 100
-  },
-  red: {
-    backgroundColor: 'red'
-  },
-  blue: {
-     backgroundColor: 'blue'
-  },
-  green: {
-    backgroundColor: 'green'
+  center:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    paddingTop: 22
   },
+  item:{
+    padding: 10,
+    fontSize: 22,
+    height: 50,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  },
+
 });
